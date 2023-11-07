741aaaa7ce0d3a57acee3f5882f04fa6f2170da6
Nishit Mangal <nishitmangal5@gmail.com>
2023-11-08 01:32:52 +0530

    To Do React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [title, setTitle] = useState("")
+  const [description, setDespription] = useState("")
+  const [allData, setAllData] = useState([]);
+
+  const onChangeTitle = (e)=>{
+    setTitle(e.target.value)
+  }
+  const onChangeDescription = (e)=>{
+    setDespription(e.target.value)
+  }
+  const onSubmitForm = (e)=>{
+    e.preventDefault();
+    if(title==="" && description===""){
+      return
+    }
+    let tempData = {
+      tempTitle: title,
+      tempDescription: description
+    }
+    setAllData([
+      ...allData,
+      tempData
+    ])
+    setTitle("")
+    setDespription("")
+  }
+  const handleDelete = (index)=>{
+      console.log("Remove clicked", index)
+      const tempArr = [...allData];
+      tempArr.splice(index, 1)
+      setAllData(tempArr)
+  }
+  
+  return(
+    <>
+      <h1>Nishit's to do list</h1>
+       
+      <form id="InputForm" onSubmit={onSubmitForm}>
+        <input
+          placeholder="Enter title"
+          type="text"
+          value={title}
+          onChange={onChangeTitle}
+          className="Box"
+        /> 
+        <input id="Description"
+          placeholder="Enter description"
+          type="text"
+          value={description}
+          onChange={onChangeDescription}
+          className="Box"
+        />
+        <button className="Addbutton">
+          Add
+        </button>
+      </form>
+      {
+        allData.map((item, index)=>{
+          return(
+            <div key={index} className="ExistingContainer">
+              <div className="Box DoneBackgroundColor">{item?.tempTitle}</div>
+              <div className="Box DoneBackgroundColor">{item?.tempDescription}</div>
+              <button 
+                className="RemoveButton"
+                onClick={()=>handleDelete(index)}
+              >Remove</button>
+            </div>
+          )
+        })
+      }
+    </>
+  )
 }
 
-export default App;
+export default App
